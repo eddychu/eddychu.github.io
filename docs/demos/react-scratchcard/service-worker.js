999f68b4301539834327cb00b832a33f156c8d22
Ahq <59189798+ahqcoding@users.noreply.github.com>
2020-09-25 10:45:12 +0800

    rebuild demo

--- docs/demos/react-scratchcard/service-worker.js
+++ docs/demos/react-scratchcard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4743b2b7de724d335aec10197d9a96a2.js"
+  "./precache-manifest.3a50611fe3590908bf5d3be97841bde2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
