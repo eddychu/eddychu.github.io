1c4c6fd293710f6edd675d95806857cfe0bdb509
eddychu <hey.yadong@gmail.com>
2021-02-26 16:05:15 +0800

    replace gatsby with simple-blog

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "./style.css"
+import "../node_modules/highlight.js/styles/github-gist.css"
+export default function MyApp({Component, pageProps}) {
+    return <Component {...pageProps} />
+}
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+
+import {getList } from "../lib/posts"
+const rootDirectory = "_posts"
+
+
+const Index = ({allPosts}) => {
+    return (
+    <div>
+        <h1>Connecting Dots</h1>
+        <hr />
+        <ul>
+        {
+            allPosts.map((post) => <li key={post.path}><a href={post.path}><h3>{post.title}</h3></a></li>)
+        }
+        </ul>
+    </div>)
+}
+export default Index;
+
+
+export async function getStaticProps() {
+    const allPosts = await getList();
+    return {
+        props: { allPosts },
+    }
+}
